4b5629a34e6df50b27454960a0a78dcecf1f4a7e
ykarakita <taf.749@gmail.com>
2022-09-11 11:16:04 +0900

    add validates to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+  validates :email, presence: true, uniqueness: true
 end

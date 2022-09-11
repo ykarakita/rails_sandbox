cf658f2cde293f363d6824036498eed2afa424f0
ykarakita <taf.749@gmail.com>
2022-09-11 11:12:31 +0900

    create users table and endpoint

---
+++ db/migrate/20220911020852_create_user.rb
@@ -0,0 +1,14 @@
+class CreateUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.integer :age, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end

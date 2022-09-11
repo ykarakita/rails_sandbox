8ae8733691dfd20a11637498004bf8f841fb7663
ykarakita <taf.749@gmail.com>
2022-09-11 13:05:47 +0900

    update user factory

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Internet.email }
+    email { Faker::Internet.email(name: "#{first_name} #{last_name}", separators: ["-"]) }
     age { rand(0...100) }
   end
 end

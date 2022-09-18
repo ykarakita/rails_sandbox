c56f4b198a4c9dec64e65c0eba8ee736c0a10e49
ykarakita <taf.749@gmail.com>
2022-09-18 13:53:40 +0900

    add rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
   def index
     res = Post.joins(:user).map { _1.attributes.merge(user: _1.user) }
--- db/migrate/20220911031251_add_index_to_user_first_name.rb
+++ db/migrate/20220911031251_add_index_to_user_first_name.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIndexToUserFirstName < ActiveRecord::Migration[7.0]
   def change
     add_index :users, :first_name

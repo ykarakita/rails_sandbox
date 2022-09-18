c540de6b70de1e54ba9798780f6cb9925af366c5
ykarakita <taf.749@gmail.com>
2022-09-18 12:49:29 +0900

    add user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validation" do
+    subject { FactoryBot.build(:user) }
+
     it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :age }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
   end
 end

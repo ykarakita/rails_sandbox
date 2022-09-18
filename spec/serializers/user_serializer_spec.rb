9ea02732a0f2522b23e80b9469860c31e05b276f
ykarakita <taf.749@gmail.com>
2022-09-18 16:04:38 +0900

    add user_serializer

---
+++ spec/serializers/user_serializer_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UserSerializer, type: :serializer do
+  let(:instance) { described_class.new(user) }
+  let!(:user) { FactoryBot.create(:user) }
+
+  it do
+    expect(instance.attributes).to eq({
+                                        id: user.id,
+                                        first_name: user.first_name,
+                                        last_name: user.last_name,
+                                        age: user.age,
+                                        email: user.email,
+                                      })
+  end
+end
